531289409ef46a79e6fde080ac1bceb2dce513d2
Ihar Shyrynkou <Ihar.Shyrynkou@gmail.com>
2023-09-12 08:39:41 +0300

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,5 +1,10 @@
 docker stop $(docker ps -qa)
+sleep 3
 docker rm $(docker ps -qa) 
+sleep 2
 docker rmi -f $(docker images -qa) 
+sleep 2
 docker volume rm $(docker volume ls -q) 
+sleep 2
 docker network rm $(docker network ls -q)
+sleep 1
